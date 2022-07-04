5dd7e0dec04bbcd417e0ad7377ca2eeaa5bfc94b
robert-muriithi <njuguiinirobert@gmail.com>
2022-07-05 01:08:43 +0300

    setting dashboard items

--- app/src/main/java/dev/robert/foodonor/fragments/HomeFragment.kt
+++ app/src/main/java/dev/robert/foodonor/fragments/HomeFragment.kt
@@ -1,10 +1,11 @@
 package dev.robert.foodonor.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import dev.robert.foodonor.R
 import dev.robert.foodonor.databinding.FragmentHomeBinding
 
@@ -14,10 +15,19 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        //(activity as AppCompatActivity).setSupportActionBar(binding)
+
+        binding.cardLogout.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
+        }
+        binding.cardDonate.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_donateFragment)
+        }
 
         return view
     }
