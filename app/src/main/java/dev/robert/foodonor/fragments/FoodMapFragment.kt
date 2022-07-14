dafa55a19c020f3dd2c6c062710cc7d3efd69da8
robert-muriithi <njuguiinirobert@gmail.com>
2022-07-14 22:20:43 +0300

    locating each donor on the foodmap and placing markers on each

--- app/src/main/java/dev/robert/foodonor/fragments/FoodMapFragment.kt
+++ app/src/main/java/dev/robert/foodonor/fragments/FoodMapFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.common.ConnectionResult
@@ -14,14 +15,18 @@ import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.LocationListener
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
 import dagger.hilt.android.AndroidEntryPoint
 import dev.robert.foodonor.R
-import dev.robert.foodonor.databinding.FragmentDonateBinding
 import dev.robert.foodonor.databinding.FragmentFoodMapBinding
 
 
@@ -30,7 +35,7 @@ class FoodMapFragment : Fragment(), OnMapReadyCallback,
     GoogleApiClient.ConnectionCallbacks,
     GoogleApiClient.OnConnectionFailedListener,
     LocationListener {
-   private lateinit var binding: FragmentFoodMapBinding
+    private lateinit var binding: FragmentFoodMapBinding
     private lateinit var mMap: GoogleMap
     private lateinit var mGoogleApiClient: GoogleApiClient
     private lateinit var mLastLocation: Location
@@ -50,15 +55,16 @@ class FoodMapFragment : Fragment(), OnMapReadyCallback,
         val view = binding.root
         auth = FirebaseAuth.getInstance()
         userID = auth.currentUser!!.uid
+        database = FirebaseFirestore.getInstance()
 
         mapFragment = childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
         if (ActivityCompat.checkSelfPermission(
                 requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
-        ){
+        ) {
             mapFragment.getMapAsync(this)
-        } else{
+        } else {
             ActivityCompat.requestPermissions(
                 requireActivity(),
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -96,20 +102,90 @@ class FoodMapFragment : Fragment(), OnMapReadyCallback,
         mMap.isMyLocationEnabled = true
     }
 
-    override fun onConnected(p0: Bundle?) {
-        TODO("Not yet implemented")
+    override fun onConnected(bundle: Bundle?) {
+        mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+            mGoogleApiClient,
+            mLocationRequest,
+            this
+        )
     }
 
     override fun onConnectionSuspended(p0: Int) {
-        TODO("Not yet implemented")
+
     }
 
     override fun onConnectionFailed(p0: ConnectionResult) {
-        TODO("Not yet implemented")
+
     }
 
     override fun onLocationChanged(p0: Location) {
-        TODO("Not yet implemented")
+        mLastLocation = p0
+        showLocation()
+        val latLng = LatLng(p0.latitude, p0.longitude)
+
+        val markerOptions1 = MarkerOptions().position(latLng).title("You are here")
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15F))
+        mMap.addMarker(markerOptions1)?.showInfoWindow()
+    }
+
+    private fun showLocation() {
+        database.collection("Donations")
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    for (document in task.result) {
+                        if (document.contains("location") && document.contains("name") && document.contains(
+                                "description"
+                            )
+                        ) {
+                            val location = document["location"] as GeoPoint?
+                            val title = document["name"] as String?
+                            val description = document["description"] as String?
+                            val latLng = LatLng(
+                                location!!.latitude, location.longitude
+                            )
+                            mMap.addMarker(
+                                MarkerOptions().position(latLng).title("$title Donor")
+                                    .snippet(description).icon(
+                                        BitmapDescriptorFactory.defaultMarker(
+                                            BitmapDescriptorFactory.HUE_GREEN
+                                        )
+                                    )
+                            )
+                        }
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "An error occurred", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mapFragment.getMapAsync(this)
+            } else {
+                Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 
