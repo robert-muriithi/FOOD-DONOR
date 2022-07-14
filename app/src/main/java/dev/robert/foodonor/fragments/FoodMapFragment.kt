7e61f92d08ad8cb973ffa89b1e6c2ea473b2b667
robert-muriithi <njuguiinirobert@gmail.com>
2022-07-14 12:55:54 +0300

    creating general food map

--- app/src/main/java/dev/robert/foodonor/fragments/FoodMapFragment.kt
+++ app/src/main/java/dev/robert/foodonor/fragments/FoodMapFragment.kt
@@ -1,17 +1,46 @@
 package dev.robert.foodonor.fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
 import dev.robert.foodonor.R
+import dev.robert.foodonor.databinding.FragmentDonateBinding
 import dev.robert.foodonor.databinding.FragmentFoodMapBinding
 
+
 @AndroidEntryPoint
-class FoodMapFragment : Fragment() {
+class FoodMapFragment : Fragment(), OnMapReadyCallback,
+    GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener,
+    LocationListener {
    private lateinit var binding: FragmentFoodMapBinding
+    private lateinit var mMap: GoogleMap
+    private lateinit var mGoogleApiClient: GoogleApiClient
+    private lateinit var mLastLocation: Location
+    private lateinit var mLocationRequest: LocationRequest
+    private val REQUEST_CODE = 11
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var auth: FirebaseAuth
+    private lateinit var userID: String
+    private lateinit var database: FirebaseFirestore
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,9 +48,68 @@ class FoodMapFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentFoodMapBinding.inflate(inflater, container, false)
         val view = binding.root
+        auth = FirebaseAuth.getInstance()
+        userID = auth.currentUser!!.uid
 
+        mapFragment = childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ){
+            mapFragment.getMapAsync(this)
+        } else{
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE
+            )
+        }
 
         return view
+    }
+
+    @Synchronized
+    protected fun buildGoogleApiClient() {
+        mGoogleApiClient = GoogleApiClient.Builder(requireContext())
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build()
+        mGoogleApiClient.connect()
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+
+        buildGoogleApiClient()
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        mMap.isMyLocationEnabled = true
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLocationChanged(p0: Location) {
+        TODO("Not yet implemented")
     }
 
 
